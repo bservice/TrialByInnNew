9b0cf5518ab3b96eebfb40d13adaaa49b87b40de
Brandon Service <brservice@comcast.net>
2020-10-22 17:54:29 -0400

    Added end scene that displays score and a button that returns you to the main menu

--- Assets/Scripts/EndDisplay.cs
+++ Assets/Scripts/EndDisplay.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class EndDisplay : MonoBehaviour
 {
+    public GameObject displayBoard;
+
     private GameUIDisplay scoreboard;
+
+    private GUIStyle style;
 
     private int score;
     private int patronsLeft;
     private int patronsSat;
+
+    private float xPos;
+    private float yPos;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +26,22 @@ public class EndDisplay : MonoBehaviour
         patronsSat = scoreboard.PatronsSat;
         Destroy(scoreboard);
         Destroy(scoreboard.gameObject);
+        xPos = Camera.main.WorldToScreenPoint(displayBoard.gameObject.transform.position).x;
+        yPos = Screen.height - Camera.main.WorldToScreenPoint(displayBoard.gameObject.transform.position).y;
+        style = new GUIStyle();
+        style.fontSize = 28;
+        style.normal.textColor = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xPos = Camera.main.WorldToScreenPoint(displayBoard.gameObject.transform.position).x;
+        yPos = Screen.height - Camera.main.WorldToScreenPoint(displayBoard.gameObject.transform.position).y;
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(100.0f, 39.0f, 22, 19), score.ToString());
-        GUI.Label(new Rect(100.0f, 59.0f, 22, 19), patronsLeft.ToString());
-        GUI.Label(new Rect(100.0f, 79.0f, 22, 19), patronsSat.ToString());
+        GUI.Label(new Rect(xPos + 115.0f, yPos + 95.0f, 22, 19), score.ToString(), style);
     }
 }
