906de3c4eaf64f67371eaa999d16108fa2851c95
Dissentinel <lightbladepaladin@gmail.com>
2020-10-27 19:22:18 -0400

    Merging

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -24,8 +24,8 @@ public class Customer : MonoBehaviour
 
     // Carson Fields
     private float tileDistance = .16f;
-    private int curX = 0;
-    private int curY = 9;
+    public int curX = 0;
+    public int curY = 9;
     private bool placed = false;
     private bool[] dir = { false, false, false, false }; // 0 = Up, 1 = Left, 2 = Down, 3 = Right
 
@@ -64,10 +64,7 @@ public class Customer : MonoBehaviour
         moveableManager = GameObject.Find("MoveableManager");
         // Carson Code
         // Call move every .3s
-        if(!isSeated)
-        {
-            InvokeRepeating("move", 0.0f, 0.15f);
-        }
+        InvokeRepeating("move", 0.0f, 0.15f);
 
 
         //Seating initialization
@@ -149,118 +146,121 @@ public class Customer : MonoBehaviour
     // Move WASD in grid
     void move()
     {
-        // UP
-        if (dir[0])
+        if(!isSeated)
         {
-            // Check if move = possible
-            if (moveCheck(pos.y + tileDistance, 1))
+            // UP
+            if (dir[0])
             {
-                // Walk Anim
-                animator.SetBool("Walk", true);
+                // Check if move = possible
+                if (moveCheck(pos.y + tileDistance, 1))
+                {
+                    // Walk Anim
+                    animator.SetBool("Walk", true);
 
-                // Move
-                pos.y += tileDistance;
-                this.transform.position = pos;
+                    // Move
+                    pos.y += tileDistance;
+                    this.transform.position = pos;
 
-                // Unfill, Move, Fill
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
-                curY--;
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
+                    // Unfill, Move, Fill
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
+                    curY--;
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
 
-                // -10 per move
-                scoreboard.Score -= 10;
+                    // -10 per move
+                    scoreboard.Score -= 10;
 
-                //Play sound every move
-                soundEffect.PlayOneShot(step);
+                    //Play sound every move
+                    soundEffect.PlayOneShot(step);
 
-              
+
+                }
             }
-        }
-        //LEFT
-        else if (dir[1])
-        {
-            // Check if move = possible
-            if (moveCheck(pos.x - tileDistance, 2))
+            //LEFT
+            else if (dir[1])
             {
-                // Walk Anim
-                animator.SetBool("Walk", true);
+                // Check if move = possible
+                if (moveCheck(pos.x - tileDistance, 2))
+                {
+                    // Walk Anim
+                    animator.SetBool("Walk", true);
 
-                // Move
-                pos.x -= tileDistance;
-                this.transform.position = pos;
+                    // Move
+                    pos.x -= tileDistance;
+                    this.transform.position = pos;
 
-                // Unfill, Move, Fill
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
-                curX--;
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
+                    // Unfill, Move, Fill
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
+                    curX--;
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
 
-                // -10 per move
-                scoreboard.Score -= 10;
+                    // -10 per move
+                    scoreboard.Score -= 10;
 
-                //Play sound every move
-                soundEffect.PlayOneShot(step);
+                    //Play sound every move
+                    soundEffect.PlayOneShot(step);
+                }
             }
-        }
-        // DOWN
-        else if (dir[2])
-        {
-            // Check if move = possible
-            if (moveCheck(pos.y - tileDistance, 3))
+            // DOWN
+            else if (dir[2])
             {
-                // Walk Anim
-                animator.SetBool("Walk", true);
+                // Check if move = possible
+                if (moveCheck(pos.y - tileDistance, 3))
+                {
+                    // Walk Anim
+                    animator.SetBool("Walk", true);
 
-                // Move
-                pos.y -= tileDistance;
-                this.transform.position = pos;
+                    // Move
+                    pos.y -= tileDistance;
+                    this.transform.position = pos;
 
-                // Unfill, Move, Fill
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
-                curY++;
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
+                    // Unfill, Move, Fill
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
+                    curY++;
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
 
-                // -10 per move
-                scoreboard.Score -= 10;
+                    // -10 per move
+                    scoreboard.Score -= 10;
 
-                //Play sound every move
-                soundEffect.PlayOneShot(step);
+                    //Play sound every move
+                    soundEffect.PlayOneShot(step);
+                }
             }
-        }
-        // RIGHT
-        else if (dir[3])
-        {
-            // Check if move = possible
-            if (moveCheck(pos.x + tileDistance, 4))
+            // RIGHT
+            else if (dir[3])
             {
-                // Walk Anim
-                animator.SetBool("Walk", true);
+                // Check if move = possible
+                if (moveCheck(pos.x + tileDistance, 4))
+                {
+                    // Walk Anim
+                    animator.SetBool("Walk", true);
 
-                // Move
-                pos.x += tileDistance;
-                this.transform.position = pos;
+                    // Move
+                    pos.x += tileDistance;
+                    this.transform.position = pos;
 
-                // Unfill, Move, Fill
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
-                curX++;
-                grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
+                    // Unfill, Move, Fill
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = true;
+                    curX++;
+                    grid.ArrayGrid[curX, curY].GetComponent<Square>().isEmpty = false;
 
-                // -10 per move
-                scoreboard.Score -= 10;
+                    // -10 per move
+                    scoreboard.Score -= 10;
 
-                //Play sound every move
-                soundEffect.PlayOneShot(step);
+                    //Play sound every move
+                    soundEffect.PlayOneShot(step);
+                }
             }
-        }
-        else
-        {
-            // Idle Anim
-            animator.SetBool("Walk", false);
-        }
+            else
+            {
+                // Idle Anim
+                animator.SetBool("Walk", false);
+            }
 
-
-        //Update moveable object coords
-        this.GetComponent<MoveableObject>().xPosition = curX;
-        this.GetComponent<MoveableObject>().yPosition = 9-curY;
+            //Update moveable object coords
+            this.GetComponent<MoveableObject>().xPosition = curX;
+            this.GetComponent<MoveableObject>().yPosition = 9 - curY;
+        }
+        
     }
 
 
@@ -367,12 +367,14 @@ public class Customer : MonoBehaviour
 
         if (collision.gameObject.tag == "Seat")
         {
-            //Set their color to blue
-            this.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.cyan);
             //Set true used in Update()
             seat = collision.gameObject;
             inSeat = true;
-            
+            if(!isSeated)
+            {
+              this.GetComponent<SpriteRenderer>().material.SetColor("_Color", Color.cyan);
+
+            }
         }
             
             //Make it so that they can't collide with other customers/tables
